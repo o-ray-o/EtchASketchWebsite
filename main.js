4f88d9ef376cf89c662d607d422cc7f79bf18353
o-ray-o <rayfong_goh@mymail.sutd.edu.sg>
2022-06-10 23:50:02 +0800

    setting up the grids and changing size of grids

---
+++ main.js
@@ -0,0 +1,20 @@
+function populateBoard(size){
+    let board = document.querySelector('.board');
+    let squares =board.querySelectorAll('div');
+    squares.forEach((div)=>div.remove());
+    board.style.gridTemplateColumns=`repeat(${size},1fr)`;
+    board.style.gridTemplateRows=`repeat(${size},1fr)`;
+
+    let amount =size*size
+    for(let i =0; i<amount ;i++){
+        let square = document.createElement('div')
+        square.style.backgroundColor="blue";
+        board.insertAdjacentElement("beforeend",square);
+        }   
+}
+
+populateBoard(16);
+
+function changeSize(input){
+    populateBoard(input);
+}
